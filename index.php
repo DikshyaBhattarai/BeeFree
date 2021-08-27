f9a44ce12e02bcbeda50acb74f0065579df0a644
DikshyaBhattarai <89525845+DikshyaBhattarai@users.noreply.github.com>
2021-08-27 12:34:23 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>ModernWear</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+
+	   <div class="container">
+        <div  class="navbar">
+        	<div class="logo">
+        		<img src="images/logo.png" width="125px">
+        	</div>
+                
+                <nav>
+                	<ul>
+                		<li><a href="">Home</a></li>
+                		<li><a href="">Product</a></li>
+                		<li><a href="">About</a></li>	
+                        <li><a href="">Contact</a></li>
+                		<li><a href="">Account</a></li>
+                	</ul>
+                </nav>
+            </div>
+            <div class="row">
+            	<div class="col-2">
+            		<h1>Give your new style..</h1>
+            		<p>Be confident on what you are wearing.<br> make your style a new design.<br>Wear your  dress to look beatiful. </p>
+            	</div>
+            	<div class="col-2">
+            		<img src="images">
+            	</div>
+            	
+            </div>
+       </div>      
+
+
+
+
+</body>
+</html>
